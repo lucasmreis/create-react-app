038df0aa323c5fd99c836ee8f5966653e8a4f66f
Lucas Reis <lucasmreis@gmail.com>
2017-03-29 00:30:34 -0300

    fable build + version

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -43,9 +43,12 @@ module.exports = function(
 
   // Setup the script rules
   appPackage.scripts = {
-    start: 'react-scripts start',
-    build: 'react-scripts build',
-    test: 'react-scripts test --env=jsdom',
+    startjs: 'react-scripts start',
+    buildjs: 'react-scripts build',
+    testjs: 'react-scripts test --env=jsdom',
+    start: 'dotnet fable npm-run startjs',
+    build: 'dotnet fable npm-run buildjs',
+    test: 'dotnet fable npm-run testjs',
     eject: 'react-scripts eject',
   };
 
@@ -136,6 +139,15 @@ module.exports = function(
     }
   }
 
+  console.log(`Installing .net dependencies using dotnet restore...`);
+  console.log();
+
+  const dotnetProc = spawn.sync('dotnet', ['restore'], { stdio: 'inherit' });
+  if (dotnetProc.status !== 0) {
+    console.error(`\`${command} ${args.join(' ')}\` failed`);
+    return;
+  }
+
   // Display the most elegant way to cd.
   // This needs to handle an undefined originalDirectory for
   // backward compatibility with old global-cli's.
@@ -151,6 +163,8 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log();
+  console.log('This is custom version of CRA featuring Fable.');
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
@@ -178,6 +192,7 @@ module.exports = function(
   console.log();
   console.log(chalk.cyan('  cd'), cdpath);
   console.log(`  ${chalk.cyan(`${displayedCommand} start`)}`);
+
   if (readmeExists) {
     console.log();
     console.log(
